835257d3149e98da66a934ca1f4a50a9f049bbd3
ktraxxi <rrkh96@mail.ru>
2021-12-08 21:09:36 +0300

    Раскидал вёрстку
    1) 08.12.2021

---
+++ Models/Address.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateServices
+{
+    public class Address
+    {
+        public Address(string city, string street, string buildingNumber, string appartmentNumber)
+        {
+            City = city;
+            Street = street;
+            BuildingNumber = buildingNumber;
+            AppartmentNumber = appartmentNumber;
+        }
+        public Address(string city, string street, string buildingNumber)
+        {
+            City = city;
+            Street = street;
+            BuildingNumber = buildingNumber;
+        }
+
+        [BsonElement("Город проживания")]
+        private string City { get; set; }
+        [BsonElement("Улица")]
+        private string Street { get; set; }
+        [BsonElement("Номер дома")]
+        private string BuildingNumber { get; set; }
+        [BsonElement("Номер квартиры")]
+        [BsonIgnoreIfNull]
+        private string AppartmentNumber { get; set; }
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateServices
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        [BsonElement("Имя")]
+        private string FirstName { get; set; }
+        [BsonElement("Фамилия")]
+        private string Surname { get; set; }
+        [BsonElement("Отчество")]
+        private string Patronymic { get; set; }
+        [BsonElement("Дата рождения")]
+        public DateTime BirthDate { get; set; }
+        [BsonElement("Электронная почта")]
+        private string Email { get; set; }
+        [BsonElement("Номер телефона / Логин")]
+        private string LoginPhoneNamber { get; set; }
+        [BsonElement("Пароль")]
+        private string Password { get; set; }
+        [BsonElement("Полный адрес")]
+        public Address FullAddress { get; set; }
+
+        [BsonElement("Серия и номер паспорта")]
+        private string PassportNumber { get; set; }
+        [BsonElement("Номер медицинского полиса")]
+        private string MedicalPolicyNumber { get; set; }
+        [BsonElement("ИНН")]
+        private string TaxIdenticalNumber { get; set; }
+        [BsonElement("СНИЛС")]
+        private string InsuranceNumber { get; set; }
+        private bool IsRegistered { get; set; }
+        private bool IsAuthorized { get; set; }
+
+
+        public void Registration()
+        {
+
+        }
+
+        public void Authorization()
+        {
+
+        }
+    }
+}
