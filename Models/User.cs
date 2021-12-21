e71638fa9694bbbaf5b1d0ce4d42b98edd54ae99
ktraxxi <rrkh96@mail.ru>
2021-12-21 19:09:01 +0300

    Перенаправление + редактирование полей

--- Models/User.cs
+++ Models/User.cs
@@ -11,8 +11,8 @@ namespace Project_State_Services.Models
     public class User
     {
         public User(string firstName, string surname, string patronymic, 
-            DateTime birthDate, string email, string loginPhoneNumber, string password, 
-            Address fullAddress, string passportNumber, string medicalPolicyNumber, 
+            string birthDate, string email, string loginPhoneNumber, string password, 
+            string passportNumber, string medicalPolicyNumber, 
             string taxIdenticalNumber, string insuranceNumber, 
             string city, string street, string buildingNumber, string appartmentNumber)
         {
@@ -38,54 +38,53 @@ namespace Project_State_Services.Models
             FirstName = firstName;
             Surname = surname;
             Patronymic = patronymic;
+            BirthDate = "нет данных";
+            Email = "нет данных";
             LoginPhoneNumber = loginPhoneNumber;
             Password = password;
+            PassportNumber = "нет данных";
+            MedicalPolicyNumber = "нет данных";
+            TaxIdenticalNumber = "нет данных";
+            InsuranceNumber = "нет данных";
+            City = "нет данных";
+            Street = "нет данных";
+            BuildingNumber = "нет данных";
+            AppartmentNumber = "нет данных";
         }
 
-        [BsonId]
-        public ObjectId _id { get; set; }
-        [BsonElement("Имя")]
-        public string FirstName { get; set; }
-        [BsonElement("Фамилия")]
-        public string Surname { get; set; }
-        [BsonElement("Отчество")]
-        public string Patronymic { get; set; }
-        [BsonElement("Дата рождения")]
-        public DateTime BirthDate { get; set; }
-        [BsonElement("Электронная почта")]
-        public string Email { get; set; }
-        [BsonElement("Номер телефона / Логин")]
-        public string LoginPhoneNumber { get; set; }
-        [BsonElement("Пароль")]
-        public string Password { get; set; }
-
-        [BsonElement("Серия и номер паспорта")]
-        public string PassportNumber { get; set; }
-        [BsonElement("Номер медицинского полиса")]
-        public string MedicalPolicyNumber { get; set; }
-        [BsonElement("ИНН")]
-        public string TaxIdenticalNumber { get; set; }
-        [BsonElement("СНИЛС")]
-        public string InsuranceNumber { get; set; }
-        [BsonElement("Город проживания")]
-        public string City { get; set; }
-        [BsonElement("Улица")]
-        public string Street { get; set; }
-        [BsonElement("Номер дома")]
-        public string BuildingNumber { get; set; }
-        [BsonElement("Номер квартиры")]
-        [BsonIgnoreIfNull]
-        public string AppartmentNumber { get; set; }
-        //private bool IsRegistered { get; set; }
-        //private bool IsAuthorized { get; set; }
-
-
+        public static bool isAuthorized;
+        [BsonId] public ObjectId _id { get; set; }
+        [BsonElement("Имя")] public string FirstName { get; set; }
+        [BsonElement("Фамилия")] public string Surname { get; set; }
+        [BsonElement("Отчество")] public string Patronymic { get; set; }
+        [BsonElement("Дата рождения")] public string BirthDate { get; set; }
+        [BsonElement("Электронная почта")] public string Email { get; set; }
+        [BsonElement("Номер телефона / Логин")] public string LoginPhoneNumber { get; set; }
+        [BsonElement("Пароль")] public string Password { get; set; }
+        [BsonElement("Серия и номер паспорта")] public string PassportNumber { get; set; }
+        [BsonElement("Номер медицинского полиса")] public string MedicalPolicyNumber { get; set; }
+        [BsonElement("ИНН")] public string TaxIdenticalNumber { get; set; }
+        [BsonElement("СНИЛС")] public string InsuranceNumber { get; set; }
+        [BsonElement("Город проживания")] public string City { get; set; }
+        [BsonElement("Улица")] public string Street { get; set; }
+        [BsonElement("Номер дома")] public string BuildingNumber { get; set; }
+        [BsonElement("Номер квартиры")] [BsonIgnoreIfNull] public string AppartmentNumber { get; set; }
+        
+        public static void IsAuthorizedTrue()
+        {
+            isAuthorized = true;
+        }
+        public static void IsAuthorizedFalse()
+        {
+            isAuthorized = false;
+        }
+        
         public static void Registration(User user)
         {
             var client = new MongoClient("mongodb://localhost");
             var db = client.GetDatabase("State_Services_Users");
             var collection = db.GetCollection<User>("Users");
-            collection.InsertOne(user);
+            collection.InsertOne(user);            
         }
         public static User Authorization(string login, string password, User user)
         {
@@ -95,13 +94,25 @@ namespace Project_State_Services.Models
             user = collection.Find(filter => filter.Password == password && filter.LoginPhoneNumber == login).FirstOrDefault();
             return user;
         }
-
         public static List<User> UserList()
         {
             var client = new MongoClient("mongodb://localhost");
             var db = client.GetDatabase("State_Services_Users");
             var collection = db.GetCollection<User>("Users");
             return collection.Find(x => true).ToList();
+        }
+        public static void Redact(string AnketaLogin, string firstName, string surname
+            //, string patronymic,
+            //string birthDate, string email, string loginPhoneNumber, string password,
+            //string passportNumber, string medicalPolicyNumber,
+            //string taxIdenticalNumber, string insuranceNumber,
+            //string city, string street, string buildingNumber, string appartmentNumber
+            )
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var db = client.GetDatabase("State_Services_Users");
+            var collection = db.GetCollection<User>("Users");
+            collection.UpdateOne(x => x.LoginPhoneNumber == AnketaLogin, Builders<User>.Update.Set(x => x.FirstName, firstName).Set(x => x.Surname, surname));
         }
     }
 }
