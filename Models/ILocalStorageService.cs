cff82746b0251f6284587469a2910a12bef36229
ktraxxi <rrkh96@mail.ru>
2022-01-28 21:02:12 +0300

    final штрихи

--- Models/ILocalStorageService.cs
+++ Models/ILocalStorageService.cs
@@ -5,13 +5,9 @@ namespace Project_State_Services.Models
     public interface ILocalStorageService
     {
         Task SetAsync<T>(string key, T item) where T : class;
-
         Task SetStringAsync(string key, string value);
-
         Task<T> GetAsync<T>(string key) where T : class;
-
         Task<string> GetStringAsync(string key);
-
         Task RemoveAsync(string key);
     }
 }
